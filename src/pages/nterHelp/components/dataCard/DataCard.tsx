34a69168c7771657bccbac0514ced3ea8179c59c
ASanchz85 <asanchz85@gmail.com>
2024-10-27 22:01:15 +0100

    FIX: getting clearer text within the content element

--- src/pages/nterHelp/components/dataCard/DataCard.tsx
+++ src/pages/nterHelp/components/dataCard/DataCard.tsx
@@ -2,17 +2,15 @@ import './dataCard.css'
 
 interface DataCardProps {
   explanationData: {
-    title: string
     content: string
   }
 }
 
 function DataCard({ explanationData }: DataCardProps) {
-  const { title, content } = explanationData
+  const { content } = explanationData
 
   return (
     <div className='data__content'>
-      <h4>{title}</h4>
       <p>{content}</p>
     </div>
   )
